c1ac291c929e044592583d59ba1b5be9bcd5686f
Khoa <khoakhoa2812@gmail.com>
2024-11-14 16:27:36 +0700

    update commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
+import { DefaultHabitsModule } from './default_habits/default_habits.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mobileApplication:7DIDNohssRKgsi7c@cluster0.cwr8e.mongodb.net/')],
+  imports: [MongooseModule.forRoot('mongodb+srv://mobileApplication:7DIDNohssRKgsi7c@cluster0.cwr8e.mongodb.net/'), DefaultHabitsModule],
   controllers: [],
   providers: [],
 })
