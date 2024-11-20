3adab74e8b944ae73bc048b510c25cf1990e6293
Nguyen Anh Thang <nganhthang147@gmail.com>
2024-11-20 22:21:03 +0700

    add more:

---
+++ src/modules/quotes/quotes.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { QuotesService } from './quotes.service';
+import { QuotesController } from './quotes.controller';
+import { QuotesSyncService } from "./quotes.sync.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Quote, QuotesSchema } from "./quotes.schema";
+import { UserModule } from "../user/user.module";
+import { HttpModule } from '@nestjs/axios';
+import { HttpService } from "../../shared/http.service";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Quote.name, schema: QuotesSchema }]),
+    UserModule,
+    HttpModule
+  ],
+  controllers: [QuotesController],
+  providers: [QuotesService,QuotesSyncService, HttpService],
+  exports: [QuotesService, QuotesSyncService, HttpService]
+})
+export class QuotesModule {}
