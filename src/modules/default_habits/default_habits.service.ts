ad4251d53cc3d6bb4ae8dac470fd4609fa9deff2
Khoa <khoakhoa2812@gmail.com>
2024-11-21 09:03:21 +0700

    create-default_habits commit

--- src/modules/default_habits/default_habits.service.ts
+++ src/modules/default_habits/default_habits.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Default_Habits } from "./schema/default_habits.schema";
 import { CreateDefault_HabitsDto } from "./dto/create-default_habits.dto";
+import { DefaultHabitResponseDto } from "./dto/create-default_habits_response.dto";
+import { plainToInstance } from "class-transformer";
 
 @Injectable()
 export class Default_HabitsService {
@@ -12,6 +14,10 @@ export class Default_HabitsService {
     console.log(createDefault_HabitsDto)
     const createdHabits = new this.Default_HabitsModel(createDefault_HabitsDto);
     console.log(createdHabits)
-    return createdHabits.save();
+    createdHabits.save();
+    return plainToInstance(DefaultHabitResponseDto, {
+      habitid: createdHabits.id,
+      status: HttpStatus.CREATED
+    })
   }
 }
