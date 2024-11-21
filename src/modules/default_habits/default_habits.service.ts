4e59fd42216347c3f021b3a9946f52bee8e97fc7
Khoa <khoakhoa2812@gmail.com>
2024-11-21 21:31:03 +0700

    create-default_habits commit

--- src/modules/default_habits/default_habits.service.ts
+++ src/modules/default_habits/default_habits.service.ts
@@ -11,9 +11,7 @@ export class Default_HabitsService {
   constructor(@InjectModel(Default_Habits.name) private Default_HabitsModel: Model<Default_Habits>) {}
 
   async createDefault_Habits(createDefault_HabitsDto: CreateDefault_HabitsDto) {
-    console.log(createDefault_HabitsDto)
     const createdHabits = new this.Default_HabitsModel(createDefault_HabitsDto);
-    console.log(createdHabits)
     createdHabits.save();
     return plainToInstance(DefaultHabitResponseDto, {
       habitid: createdHabits.id,
